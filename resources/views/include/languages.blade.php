bb5a11dd6937f9f8e7a10d186f70b640029ccf47
andrey-tarasenko <andrey.tarasenko@finsight.com>
2019-01-16 20:04:30 +0200

    VT-2 language selector scss

--- resources/views/include/languages.blade.php
+++ resources/views/include/languages.blade.php
@@ -7,29 +7,31 @@
     </select>
 </div>  --}}
 
-<a href="#" id="userLanguages" class="user-balances" data-toggle="dropdown" aria-haspopup="true">
-    @foreach(trans('app.languages') as $lang => $title)
-        @if($lang == Lang::getLocale())
-        <i class="text-secondary"></i>
-            <img src="{{asset('img/flags/'.$lang.'.png')}}" alt="Language" class="lang__flag-pic" style="height: 20px;">
-            <small class="text-muted desc">{{$title}} </small>
-        <i class="icon-chevron-small-down"></i>
-        @endif
-    @endforeach
-</a>
-
-<div class="dropdown-menu dropdown-menu-right lg" aria-labelledby="userLanguages">
-    <ul class="stats-widget languages">
+<div class="dropdown choose_lang show">
+    <a href="#" id="userLanguages" class="user-balances" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
         @foreach(trans('app.languages') as $lang => $title)
-            <li class="{{$lang == Lang::getLocale() ? 'active ' : ''}}my-1">
-                {{--  <a href="{{lang_toggle_href($lang)}}">{{$title}}</a>  --}}
-                <a href="{{ route(\Request::route()->getName(), ['lang' => $lang]) }}">
-                    <p class="text-muted desc">
-                        <img src="{{asset('img/flags/'.$lang.'.png')}}" alt="Language" class="lang__flag-pic" style="height: 20px;">
-                        {{$title}}
-                    </p>
-                </a>
-            </li>
+            @if($lang == Lang::getLocale())
+            <i class="text-secondary"></i>
+                <img src="{{asset('img/flags/'.$lang.'.png')}}" alt="Language" class="lang__flag-pic" style="height: 20px;">
+                <small class="text-muted desc">{{$title}} </small>
+            <i class="icon-chevron-small-down"></i>
+            @endif
         @endforeach
-    </ul>
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right lg show" aria-labelledby="userLanguages" x-placement="bottom-end" style="position: absolute; transform: translate3d(-143px, 66px, 0px); top: 0px; left: 0px; will-change: transform;">
+        <ul class="stats-widget languages">
+            @foreach(trans('app.languages') as $lang => $title)
+                <li class="{{$lang == Lang::getLocale() ? 'active ' : ''}}my-1">
+                    {{--  <a href="{{lang_toggle_href($lang)}}">{{$title}}</a>  --}}
+                    <a href="{{ route(\Request::route()->getName(), ['lang' => $lang]) }}">
+                        <p class="text-muted desc">
+                            <img src="{{asset('img/flags/'.$lang.'.png')}}" alt="Language" class="lang__flag-pic" style="height: 20px;">
+                            {{$title}}
+                        </p>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </div>
