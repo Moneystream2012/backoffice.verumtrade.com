b01afe284f20d7290d5710f8e5d81d0b68b9e784
andrey-tarasenko <andrey.tarasenko@finsight.com>
2019-01-18 16:24:14 +0200

    VT-2 replenish-form added tagged+

--- resources/views/unify/layouts/personal-office.blade.php
+++ resources/views/unify/layouts/personal-office.blade.php
@@ -421,73 +421,49 @@
                     </div>
                 </div>
 
-                <div class="row">
-                    <form id="replenishment_in_layout" action="https://backoffice.verumtrade.com/personal-office/replenishment" method="post">
-                    {{--Form::open(['route'=> 'personal-office/replenishment.post'])--}}
 
-                    <!--div class="col-sm-4">
-                        <div class="form-group">
-                            <label for="amount" class="control-label">@ lang('personal-office/replenishment.amount') <span id="amount_currency"></span></label>
-                            <input type="text" class="form-control money" placeholder="0.00" name="amount" required autocomplete="off"/>
-                        </div>
-                    </div--->
-
-                    <div class="replenish">
-                        <div class="replenish_title">
-                            Пополнение баланса
-                        </div>
-
-                        <div class="summ">
-                            @lang('personal-office/replenishment.amount')
-                            <label>
-                                <input type="text" class="" placeholder="0.00" name="replenishment_amount" required autocomplete="off"/>
-                                <span>USD</span>
-                            </label>
-                        </div>
-
-                        <div class="method">
-                            <label for="replenishment_method" class="control-label">@lang('personal-office/replenishment.replenishment_method')</label>
-                            <select name="replenishment_method" id="replenishment_method" required>
-                                <option value="">@lang('personal-office/replenishment.select_option')</option>
-                                <option value="bitcoin">@lang('personal-office/replenishment.bitcoin')</option>
-                                <option value="perfect_money">@lang('personal-office/replenishment.perfect_money')</option>
-                                <option value="free_kassa">@lang('personal-office/replenishment.free_kassa')</option>
-
-                            </select>
-                        </div>
-                    </div>
-
-                    <button class="btn btn_green" type="submit">
-                        @lang('personal-office/replenishment.title')
-                    </button>
-
-                    {{-- Form::close() --}}
-                    </form>
-                </div>
-
-                --}}
-
-                <!--div class="replenish">
+                <div class="replenish">
                     <div class="replenish_title">
                         Пополнение баланса
                     </div>
+
+                    <!--div class="col-sm-4">
+<div class="form-group">
+    <label for="amount" class="control-label">@ lang('personal-office/replenishment.amount') <span id="amount_currency"></span></label>
+    <input type="text" class="form-control money" placeholder="0.00" name="amount" required autocomplete="off"/>
+</div>
+</div--->
+                    {{--Form::open(['route'=> 'personal-office/replenishment.post'])--}}
+                    <form id="replenishment_in_layout" action="https://backoffice.verumtrade.com/personal-office/replenishment" method="post">
+
                     <div class="summ">
-                        Сумма пополнения
+                        @lang('personal-office/replenishment.amount')
                         <label>
-                            <input type="text">
+                            <input type="text" class="" placeholder="0.00" name="replenishment_amount" required autocomplete="off"/>
                             <span>USD</span>
                         </label>
                     </div>
+
                     <div class="method">
-                        Метод
-                        <select>
-                            <option value="Visa/Mastercard">Visa/Mastercard</option>
-                            <option value="Visa/Mastercard">Mastercard</option>
-                            <option value="Visa/Mastercard">Visa</option>
+                        <label for="replenishment_method" class="control-label">@lang('personal-office/replenishment.replenishment_method')</label>
+                        <select name="replenishment_method" id="replenishment_method" required>
+                            <option value="">@lang('personal-office/replenishment.select_option')</option>
+                            <option value="bitcoin">@lang('personal-office/replenishment.bitcoin')</option>
+                            <option value="perfect_money">@lang('personal-office/replenishment.perfect_money')</option>
+                            <option value="free_kassa">@lang('personal-office/replenishment.free_kassa')</option>
+
                         </select>
                     </div>
-                    <button class="btn btn_green">ПОПОЛНИТЬ</button>
-                </div-->
+
+                        <button class="btn btn_green" type="submit">
+                            @lang('personal-office/replenishment.title')
+                        </button>
+
+                        {{-- Form::close() --}}
+                    </form>
+                </div>
+
+
                 <!-- END: .main-heading -->
                 <!-- BEGIN .main-content -->
                 <div class="main-content">
