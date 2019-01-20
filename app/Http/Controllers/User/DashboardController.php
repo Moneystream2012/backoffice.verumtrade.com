e72e4cd953b8ac865edfee86b065a92ea78d982b
oho <horishnyy@gmail.com>
2019-01-20 18:44:42 +0200

    debug

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -6,17 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Jobs\User\RankRemuneration;
 use App\Trading;
 use ConsoleTVs\Charts\Facades\Charts;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        logger($request->session()->all());
         $user = auth()->user();
         $deposits = $user->deposits()->where('active', true)->select('invest')->get();
-        $tradings= $user->tradings()->where('status', Trading::ACTIVE)->select('invest')->get();
+        $tradings = $user->tradings()->where('status', Trading::ACTIVE)->select('invest')->get();
 
         return view('unify.personal-office.dashboard', [
-            'chart'          => $this->get_chart_price_vmc(),
+            'chart' => $this->get_chart_price_vmc(),
             'deposits' => [
                 'invest' => $deposits->sum('invest'),
                 'count' => $deposits->count(),
@@ -36,15 +38,15 @@ class DashboardController extends Controller
             ->colors(['#4266b2', '#efa406'])
             ->labels(['07.11.2017', '08.11.2017', '10.01.2018', '04.02.2018', '05.02.2018', '11.03.2018'])
             ->dataset('USD', [0.10, 0.11, 0.12, 0.13, 0.14, config('mlm.currencies.VMC.USD')])
-            ->dataset('BTC', [0.00005, 0.00006, 0.00007, 0.00008,0.00009, config('mlm.currencies.VMC.BTC')]);
+            ->dataset('BTC', [0.00005, 0.00006, 0.00007, 0.00008, 0.00009, config('mlm.currencies.VMC.BTC')]);
         return $chart;
     }
 
     private function get_next_rank($user)
     {
         $next_rank = [
-            'binary'    => 0,
-            'direct'    => 0,
+            'binary' => 0,
+            'direct' => 0,
             'text_rank' => null,
             'reward' => 0,
         ];
@@ -58,7 +60,7 @@ class DashboardController extends Controller
         $next_rank['direct'] = $turnover_direct > 0 ? $turnover_direct : 0;
         $turnover_binary = $premium_rank['turnover_binary'] - $user->binary_lower_branch;
         $next_rank['binary'] = $turnover_binary > 0 ? $turnover_binary : 0;
-        $next_rank['text_rank'] = trans('rank.'.$rank);
+        $next_rank['text_rank'] = trans('rank.' . $rank);
         $next_rank['reward'] = $premium_rank['reward'];
 
         return $next_rank;
