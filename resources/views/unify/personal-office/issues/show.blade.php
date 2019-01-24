8100c6402d082e8a9b556010290dcaa792d3b4f2
oho <horishnyy@gmail.com>
2019-01-24 21:24:33 +0200

    fixes

--- resources/views/unify/personal-office/issues/show.blade.php
+++ resources/views/unify/personal-office/issues/show.blade.php
@@ -28,7 +28,7 @@
                                     <img
                                         src="{{$item->is_support ?  asset('/img/avatars/support.png') : $auth->avatar_url }}"
                                         alt="{{$item->full_name}}">
-                                    <div class="chats-name">{{ $item->is_support ? @lang('unify/personal-office/issues/show.support') : @lang('unify/personal-office/issues/show.you')}}</div>
+                                    <div class="chats-name">{{ $item->is_support ? trans('unify/personal-office/issues/show.support') : trans('unify/personal-office/issues/show.you')}}</div>
                                 </div>
                                 <div class="chats-text">{!! $item->body !!}</div>
                                 <div class="chats-hour">{{$item->created_at->diffForHumans()}}</div>
