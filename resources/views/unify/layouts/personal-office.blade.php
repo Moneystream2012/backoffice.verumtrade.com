3b00893334278f6e2e1ee2985b828fa55e2afb5b
Moneystream2012 <andrey.installmonster@gmail.com>
2019-01-25 10:55:08 +0200

    VT-2 header try fixed

--- resources/views/unify/layouts/personal-office.blade.php
+++ resources/views/unify/layouts/personal-office.blade.php
@@ -41,7 +41,7 @@
         <header class="app-header">
             <div class="container-fluid">
                 <div class="row_header">
-                    <div class="left-fixed">
+                    {{--<div class="left-fixed">
                         <div class="nav_burger">
                             <a class="mini-nav-btn float-left" href="#" id="app-side-mini-toggler">
                                 <i class="icon-sort"></i>
@@ -133,7 +133,7 @@
                         </div>
                     </div>
                 </div>
-{{--
+--}}
                 <div class="row gutters">
                     <div class="col-2 col-sm-4 col-lg-5">
                         <a class="mini-nav-btn float-left" href="#" id="app-side-mini-toggler">
@@ -195,7 +195,7 @@
                                                 <small class="text-primary">USD</small>
                                             </h5>
                                             <p>{{$l_lang->balance}} BTC</p>
-                                        </li>-- }}
+                                        </li>--}}
                                     </ul>
                                 </div>
                             </li>
@@ -223,7 +223,7 @@
                                                 </div>
                                                 <p>@lang('unify/personal-office/settings.title')</p>
                                             </a>
-                                        </li> -- }}
+                                        </li> --}}
                                         <li>
                                             <a href="{{route('personal-office.history')}}">
                                                 <div class="icon yellow">
@@ -253,7 +253,6 @@
                         </ul>
                     </div>
                 </div>
-                --}}
             </div>
         </header>
         <!-- END: .app-heading -->
